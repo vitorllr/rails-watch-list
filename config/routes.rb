725bd129f8d9d28646cb389822007a7cf50438de
Vitor Rocha <vitorlopeslrocha@gmail.com>
2023-02-27 14:52:40 -0300

    Models

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :bookmark, only: [:new, :create, :destroy] do
+    resources :list, only:[:index, :show, :new, :create] do
+      resources :movie
+    end
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
